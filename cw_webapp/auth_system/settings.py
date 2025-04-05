30c492b9a88e7a62570b8b0d4b5592162149451a
Naman <144701970+Naman-56-56@users.noreply.github.com>
2025-04-05 17:21:14 +0530

    Update settings.py

--- cw_webapp/auth_system/settings.py
+++ cw_webapp/auth_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$&r95^u$=-8t_d9ca*am%fp^6_1l_qd$0gtht_h#zwfmmr$0os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://codeweave-2.onrender.com']
 
 
 # Application definition
