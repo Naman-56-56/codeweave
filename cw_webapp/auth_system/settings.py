5e1afbcff6166116386562418f1dc8ae12f62ed4
Naman <144701970+Naman-56-56@users.noreply.github.com>
2025-04-06 20:48:50 +0530

    Update settings.py

--- cw_webapp/auth_system/settings.py
+++ cw_webapp/auth_system/settings.py
@@ -87,12 +87,24 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Use PostgreSQL if DATABASE_URL is set 
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config(
+        default=os.environ['DATABASE_URL'],
+        conn_max_age=600,
+        ssl_require=True
+    )
+
 
 
 # Password validation
